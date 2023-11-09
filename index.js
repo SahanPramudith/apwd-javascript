8bd356e6d10c640b7249865efcb23806d4062fcf
sahanpramudith <pramuditsahan466@gmail.com>
2023-11-09 19:25:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("bogahawatta");
+// console.log("matara");
+    // console.log("matara");
+    // console.log("matara");
+    // console.log("matara");
+    // console.log("matara");
+    // alert("sahan");
+    document.writeln('MAtara');
+    // document.writeln('MAtara');
+    document.write("<br>");
+    document.write("<h1>Gandara");
+    document.write('<br>');
+    // document.write('<button> click</button');
+    document.write('<br>');
+    document.writeln("sahan");
+    document.write('<br>');
+    document.write("pramudith");
+    document.write('<br>');
+    document.write("20");
+    document.write('<br>');
+    document.write('sahan <br> pramudth<br> 20 ');
+
+    
+    // document.write('<button> click</button');
+
+    // var name ='pradeep';
+
+    // {
+    //     var town ="matara";
+    // }
+    d
+    
