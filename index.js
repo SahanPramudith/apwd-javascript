310c5e3b35927bcecb825b7f71ce6f8df1702b24
sahanpramudith <pramuditsahan466@gmail.com>
2023-11-22 14:50:25 +0530

    new

--- index.js
+++ index.js
@@ -28,5 +28,71 @@ console.log("bogahawatta");
     // {
     //     var town ="matara";
     // }
-    d
+
+    // array
+
+    const array =["sahan ","chamikara"];
+
+        // var
+    //  readiclarya can
+    var a = "sahan"
+    var a = " pradeep"
+    console.log(a);
+
+    {
+        var name = "kapugama"
+    }
+    console.log(  name);
+
+    // can re assing
+    var a = 100
+    console.log(a);
+
+    // let
+    let bm = 1000
+    console.log(bm);
+    // // can't re diclarya
+    // let x = "50"
+    // let x ="60"
+    // {
+    //     let q = "700"
+    // }
+    // console.log(q);
+    bm = 2000
+    console.log(bm);
+
+    // const
+      
+    const sahan = 8000
+    // const sahan = 20000
+
+    // {
+    //     const pramudith = 500
+    // }
+    // console.log(pramudith);
+
+    // sahan = 5002
+    // console.log(sahan);
+
+    const l =10
+    const w =20
+    const addition = l+w
+    console.log(addition);
+
+    var q = 500
+    var o = 100
+    var subtrction = o-q
+    console.log(subtrction);
+
+    let c =100
+    c++
+    console.log(c);
+
+    let x = 100
+    x--
+    console.log(x);
+
     
+
+
+
